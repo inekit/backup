8022d7405e84a70b79fedf217d533640362330ff
inekit <nicklzx@uni-dubna.ru>
2022-06-30 13:10:42 +0300

    fixed admin check

--- Utils/statistics.js
+++ Utils/statistics.js
@@ -5,7 +5,7 @@ class Statistics {
     async increaseCart(userId){
         (await tOrmCon).query(
             `INSERT INTO navigator.statistics (date,users_per_day,cart_per_day) 
-            SELECT CAST(now() AS DATE), 0,0 FROM navigator.users u WHERE (CAST(now() AS DATE)<>lastUse OR lastUse IS NULL) and id = 1 LIMIT 1
+            SELECT CAST(now() AS DATE), 0,0 FROM navigator.users u WHERE (CAST(now() AS DATE)<>lastUse OR lastUse IS NULL) and id = ? LIMIT 1
             ON DUPLICATE KEY UPDATE cart_per_day = cart_per_day+1`, 
             [userId])
         .catch((e)=>{
